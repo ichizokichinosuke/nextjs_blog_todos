9f6e747d3ca6a981774dd9bb4d2244cf5bc8db85
ichizokichinosuke <nuuuukemw@gmail.com>
2022-04-26 03:15:02 +0900

    commit to vercel

--- pages/task-page.js
+++ pages/task-page.js
@@ -1,11 +1,39 @@
+import useSWR from "swr";
 import Link from "next/link";
-import Layout from "../components/Layout";
+import { useEffect } from "react";
 
-export default function TaskPage() {
+import Task from "../components/Task";
+import Layout from "../components/Layout";
+import TaskForm from "../components/TaskForm";
+import { getAllTasksData } from "../lib/tasks";
+import StateContextProvider from "../context/StateContext";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`;
+
+export default function TaskPage({ staticfilteredTasks }) {
+  const { data: tasks, mutate } = useSWR(apiUrl, fetcher, {
+    fallbackData: staticfilteredTasks,
+  });
+
+  const filteredTasks = tasks?.sort(
+    (a, b) => new Date(b.created_at) - new Date(a.created_at)
+  );
+
+  useEffect(() => {
+    mutate();
+  }, []);
+
   return (
-    <Layout title="Task page">
-        <Link href="/main-page">
-            <div className="flex cursor-pointer mt-12">
+      <StateContextProvider>
+        <Layout title="Task page">
+            <TaskForm taskCreated={mutate} />
+            <ul>
+                {filteredTasks &&
+                filteredTasks.map((task) => <Task key={task.id} task={task} taskDeleted={mutate}/>)}
+            </ul>
+            <Link href="/main-page">
+                <div className="flex cursor-pointer mt-12">
                 <svg
                     className="w-6 h-6 mr-3"
                     fill="none"
@@ -21,8 +49,17 @@ export default function TaskPage() {
                     ></path>
                 </svg>
                 <span>Back to main page</span>
-            </div>
-        </Link>
-    </Layout>
+                </div>
+            </Link>
+        </Layout>
+      </StateContextProvider>
   );
+}
+
+export async function getStaticProps() {
+  const staticfilteredTasks = await getAllTasksData();
+  return {
+    props: { staticfilteredTasks },
+    revalidate: 3,
+  };
 }
