8c5fa8b44a1680bac08aa600559aaa6eb90c6267
ichizokichinosuke <nuuuukemw@gmail.com>
2022-04-26 22:52:50 +0900

    add sitemap w next-sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+// sitemap.config.js
+module.exports = {
+    siteUrl: 'https://nextjs-blog-todos-chi-ten.vercel.app/',
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    outDir: './out',
+  };
--- pages/blog-page.js
+++ pages/blog-page.js
@@ -40,4 +40,3 @@ export async function getStaticProps() {
         revalidate: 3,
     };
 }
-
