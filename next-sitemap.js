22cd3f849c2094d284cede9e6f8a93dea526a7c0
ichizokichinosuke <nuuuukemw@gmail.com>
2022-06-12 15:35:17 +0900

    some changes

--- next-sitemap.js
+++ next-sitemap.js
@@ -3,5 +3,5 @@ module.exports = {
     siteUrl: 'https://nextjs-blog-todos-chi-ten.vercel.app/',
     generateRobotsTxt: true,
     sitemapSize: 7000,
-    outDir: './out',
+    // outDir: './out',
   };
